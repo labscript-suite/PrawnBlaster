78c6a90ff6cc2cf1ad78a4e3ac2f348de3c3c8e6
Phil Starkey <philipstarkey@users.noreply.github.com>
2021-02-07 00:05:09 +1100

    Added clock configuration options
    Default clock now configured to 100MHz (so that we have 10ns increments).
    Added serial command to reconfigure clock on the fly
    Serial command also supports switching to external clock input on GPIO 20 or 22 (and a 48MHz clock is output on GPIO 21 for debugging this - aka, it can be easily jumpered to GPIO 20)
    
    TODO: RP2040 datasheet says using resus trigger to switch clocks is not meant for production. But I can't get it to switch clk_sys clocks correctly outside of resus!
    Also, resus is not firing if running from an external clock and that clock stops. In that scenario, the entire board locks up (except the PIO core keeps executing, but really slow and may get stuck repeating until a clock is restored - but the serial input is still locked up...needs more investigating!)

--- prawnblaster/prawnblaster.c
+++ prawnblaster/prawnblaster.c
@@ -24,6 +24,11 @@
 #include "pico/stdlib.h"
 #include "pico/multicore.h"
 #include "hardware/pio.h"
+#include "hardware/pll.h"
+#include "hardware/clocks.h"
+#include "hardware/structs/pll.h"
+#include "hardware/structs/clocks.h"
+
 #include "pseudoclock.pio.h"
 
 #define DEBUG 1
@@ -50,6 +55,20 @@ int status;
 #define RUNNING 1
 #define ABORTING 2
 #define ABORTED 3
+
+// Clock status flag
+int clock_status;
+#define INTERNAL 0
+#define EXTERNAL 1
+
+// Frequency variables
+volatile bool resus_complete;
+volatile bool resus_expected;
+unsigned int _src;  // 0 = internal, 1=GPIO pin 20, 2=GPIO pin 22
+unsigned int _freq; // in Hz
+unsigned int _vcofreq;
+unsigned int _div1;
+unsigned int _div2;
 
 void core1_entry()
 {
@@ -111,24 +130,24 @@ void core1_entry()
             {
                 tight_loop_contents();
             }
-            
+
             if (status != ABORTING)
             {
                 unsigned int wait = pio_sm_get_blocking(pio, sm);
-                
+
                 // Check for timeout wait.
                 // This integer equals 2^32-1 - aka the biggest number and what happens when you subtract one from 0 (it wraps around)
                 // Note that we do not need to
-                if (wait == 4294967295) 
+                if (wait == 4294967295)
                 {
-                    waits[i] = 0; // decrement happens regardless of jump condition in PIO program. 
+                    waits[i] = 0; // decrement happens regardless of jump condition in PIO program.
                 }
                 else
                 {
-                    // This does not need a -1 offset like above since we jumped on pin change, and did not unecessarily hit an extra 
+                    // This does not need a -1 offset like above since we jumped on pin change, and did not unecessarily hit an extra
                     // jump+decrement PIO instruction. However, we do need to double it to put it back in terms of the clock frequency
                     // since the wait loop is 2 PIO instructions (the wait length was halved when written using the "set" serial command)
-                    waits[i] = wait*2; 
+                    waits[i] = wait * 2;
                 }
             }
         }
@@ -246,6 +265,82 @@ void check_status()
     }
 }
 
+void measure_freqs(void)
+{
+    // From https://github.com/raspberrypi/pico-examples under BSD-3-Clause License
+    uint f_pll_sys = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_PLL_SYS_CLKSRC_PRIMARY);
+    uint f_pll_usb = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_PLL_USB_CLKSRC_PRIMARY);
+    uint f_rosc = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_ROSC_CLKSRC);
+    uint f_clk_sys = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_SYS);
+    uint f_clk_peri = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_PERI);
+    uint f_clk_usb = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_USB);
+    uint f_clk_adc = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_ADC);
+    uint f_clk_rtc = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_RTC);
+
+    printf("pll_sys = %dkHz\n", f_pll_sys);
+    printf("pll_usb = %dkHz\n", f_pll_usb);
+    printf("rosc = %dkHz\n", f_rosc);
+    printf("clk_sys = %dkHz\n", f_clk_sys);
+    printf("clk_peri = %dkHz\n", f_clk_peri);
+    printf("clk_usb = %dkHz\n", f_clk_usb);
+    printf("clk_adc = %dkHz\n", f_clk_adc);
+    printf("clk_rtc = %dkHz\n", f_clk_rtc);
+}
+
+void resus_callback(void)
+{
+    // If we were not expecting a resus, switch back to internal clock
+    if (!resus_expected)
+    {
+        // kill external clock pin
+        gpio_set_function((_src == 2 ? 22 : 20), GPIO_FUNC_NULL);
+
+        // reinitialise pll
+        pll_init(pll_sys, 1, 1200, 6, 2);
+
+        // Configure internal clock
+        clock_configure(clk_sys,
+                        CLOCKS_CLK_SYS_CTRL_SRC_VALUE_CLKSRC_CLK_SYS_AUX,
+                        CLOCKS_CLK_SYS_CTRL_AUXSRC_VALUE_CLKSRC_PLL_SYS,
+                        100 * MHZ,
+                        100 * MHZ);
+
+        // update clock status
+        clock_status = INTERNAL;
+    }
+    else if (_src > 0)
+    {
+        clock_configure_gpin(clk_sys, (_src == 2 ? 22 : 20), _freq, _freq);
+        // update clock status
+        clock_status = EXTERNAL;
+    }
+    else
+    {
+        // Reconfigure PLL sys back to the default state of 1500 / 6 / 2 = 125MHz
+        // pll_init(pll_sys, 1, 1200 * MHZ, 6, 2);
+        pll_init(pll_sys, 1, _vcofreq, _div1, _div2);
+
+        // CLK SYS = PLL SYS (125MHz) / 1 = 125MHz
+        clock_configure(clk_sys,
+                        CLOCKS_CLK_SYS_CTRL_SRC_VALUE_CLKSRC_CLK_SYS_AUX,
+                        CLOCKS_CLK_SYS_CTRL_AUXSRC_VALUE_CLKSRC_PLL_SYS,
+                        _freq,
+                        _freq);
+
+        // update clock status
+        clock_status = INTERNAL;
+    }
+
+    // Reconfigure uart as clocks have changed
+    stdio_init_all();
+    printf("Resus event fired\n");
+
+    // Wait for uart output to finish
+    uart_default_tx_wait_blocking();
+
+    resus_complete = true;
+}
+
 void loop()
 {
     // fgets(readstring, 255, stdin);
@@ -260,7 +355,11 @@ void loop()
     }
     else if (strncmp(readstring, "status", 6) == 0)
     {
-        printf("%d\n", status);
+        printf("%d %d\n", status, clock_status);
+    }
+    else if (strncmp(readstring, "getfreqs", 8) == 0)
+    {
+        measure_freqs();
     }
     else if (strncmp(readstring, "abort", 5) == 0)
     {
@@ -286,10 +385,84 @@ void loop()
     {
         printf("Cannot execute command %s during buffered execution. Check status first and wait for it to return 0.\n", readstring);
     }
-    else if (strncmp(readstring, "readwaits", 9) == 0)
+    else if (strncmp(readstring, "setclock", 8) == 0)
+    {
+        unsigned int src;       // 0 = internal, 1=GPIO pin 20, 2=GPIO pin 22
+        unsigned int freq;      // in Hz (up to 133 MHz)
+        unsigned int vcofreq;   // in Hz (between 400MHz and 1600MHz)
+        unsigned int div1;      // First PLL divider (between 1-7)
+        unsigned int div2;      // Second PLL divider (between 1-7)
+                                // Note: freq should equal vcofreq / div1 / div2
+        int parsed = sscanf(readstring, "%*s %u %u %u %u %u", &src, &freq, &vcofreq, &div1, &div2);
+        if (parsed < 5)
+        {
+            printf("invalid request\n");
+        }
+        else
+        {
+            printf("Got request mode=%u, freq=%u MHz, vco_freq=%u MHz, div1=%u, div2=%u\n", src, freq / MHZ, vcofreq / MHZ, div1, div2);
+            if (src > 2)
+            {
+                printf("invalid request\n");
+            }
+            else
+            {
+                // Do validation checking on values provided
+                if (freq > 133 * MHZ)
+                {
+                    printf("Invalid clock frequency specified\n");
+                    return;
+                }
+                else if (src == 0 && (vcofreq < (400 * MHZ) || vcofreq > (1600 * MHZ)))
+                {
+                    printf("Invalid VCO frequency specified\n");
+                    return;
+                }
+                else if (src == 0 && (div1 < 1 || div1 > 7))
+                {
+                    printf("Invalid POSTDIV1 PLL divider specified\n");
+                    return;
+                }
+                else if (src == 0 && (div2 < 1 || div2 > 7))
+                {
+                    printf("Invalid POSTDIV2 PLL divider specified\n");
+                    return;
+                }
+
+                unsigned int old_src = _src;
+                _src = src;
+                _freq = freq;
+                _vcofreq = vcofreq;
+                _div1 = div1;
+                _div2 = div2;
+
+                // reset seen resus flag
+                resus_complete = false;
+                resus_expected = true;
+
+                if (old_src > 0)
+                {
+                    // cancel clock input on this pin to trigger resus (and thus reconfigure)
+                    gpio_set_function((old_src == 2 ? 22 : 20), GPIO_FUNC_NULL);
+                }
+                else
+                {
+                    // Break PLL to trigger resus (and thus reconfigure)
+                    pll_deinit(pll_sys);
+                }
+
+                // Wait for resus to complete
+                while (!resus_complete);
+                resus_expected = false;
+
+                printf("ok\n");
+            }
+        }
+    }
+    else if (strncmp(readstring, "getwaits", 8) == 0)
     {
         // Note that these are not the lengths of the waits, but how many base (system) clock ticks were left
-        // before timeout. 0 = timeout. a wait with a timeout of 8, and a value reported here as 2, means the 
+        // before timeout. 0 = timeout. a wait with a timeout of 8, and a value reported here as 2, means the
         // wait was 6 clock ticks long.
         printf("%u %u %u %u\n", waits[0], waits[1], waits[2], waits[3]);
     }
@@ -414,6 +587,28 @@ void loop()
 
 int main()
 {
+
+    // configure resus callback that will reconfigure the clock for us
+    // either when we change clock settings or when the clock fails (if external)
+    clocks_enable_resus(&resus_callback);
+
+    // initial clock configuration
+    _src = 0;
+    _freq = 100*MHZ;
+    _vcofreq = 1200*MHZ;
+    _div1 = 6;
+    _div2 = 2;
+    // reset seen resus flag
+    resus_complete = false;
+    resus_expected = true;
+    pll_deinit(pll_sys);
+    // Wait for resus to complete
+    while (!resus_complete);
+    resus_expected = false;
+
+    // Temp output 48MHZ clock for debug
+    clock_gpio_init(21, CLOCKS_CLK_GPOUT0_CTRL_AUXSRC_VALUE_CLK_USB, 1);
+
     stdio_init_all();
 
     multicore_launch_core1(core1_entry);
